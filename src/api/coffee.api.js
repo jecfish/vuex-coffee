d50a844ce1b259485590c755844463c8a5b377fb
chybie <chy.bie@hotmail.com>
2019-01-27 21:58:24 +0000

    change url

--- src/api/coffee.api.js
+++ src/api/coffee.api.js
@@ -2,7 +2,8 @@ import list from '../../public/static/list.json'
 
 export default {
   getList() {
-    const url = 'static/list.json'
+    // const url = 'static/list.json'
+    const url = 'https://demo2466260.mockable.io/list'
     return fetch(url).then(res => res.json())
   },
 
